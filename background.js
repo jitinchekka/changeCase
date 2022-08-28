a7ee8b72e9edff367b177ff4b5df7d5d7861f64b
rfm-jitin <jitinchekka2@gmail.com>
2022-08-28 16:08:07 +0530

    added sentence case

--- background.js
+++ background.js
@@ -67,4 +67,38 @@ chrome.runtime.onInstalled.addListener(function () {
     },
     id: "lowercase", 
   });
+  // Add another context menu item to change the case of the selected text to Sentence case
+  chrome.contextMenus.create({
+    title: "Sentence case",
+    contexts: ["selection"],
+    onclick: function (info, tab) {
+      chrome.tabs.executeScript(
+        {
+          code: "window.getSelection().toString()",
+        },
+        function (selection) {
+          // Convert the selection to uppercase
+          var text = selection[0];
+          var sentenceCase = text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+          // Replace the selection with the uppercase text
+          chrome.tabs.executeScript(
+            {
+              code: "document.getSelection().toString()",
+            },
+            function (selection) {
+              chrome.tabs.executeScript(
+                {
+                  code: "document.execCommand('insertText', false, '" + sentenceCase + "')",
+                },
+                function (selection) {
+                  console.log(selection);
+                }
+              );
+            }
+          );
+        }
+      );
+    },
+    id: "sentencecase",
+  });
 })
